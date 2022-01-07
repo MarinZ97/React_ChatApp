c3fb8e79ef5a23f83c060994f852106fb92a49b0
Marin <ddzuna@gmail.com>
2022-01-07 18:21:03 +0100

    Dodan Input, zavrsene poruke

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import './App.css';
 import Messages from './components/Messages';
 import Input from './components/Input';
-import { randomColor, randomName } from './components/NameColorData'; 
+import {randomName, randomColor} from './components/NameColorData'; 
 
 
 class App extends Component {
@@ -14,13 +14,47 @@ class App extends Component {
     }
   }
 
- 
+  constructor() {
+    super();
+    this.drone = new window.Scaledrone('5D9V0tsX5DxmjSvr', {data: this.state.member});
+    console.log("Drone prikaz:", this.drone)
+    this.drone.on('open', error => {
+      if (error) {
+        return console.error(error);
+      }
+      console.log("Test", this.drone)
+      const member = {...this.state.member};
+      member.id = this.drone.clientId;
+      this.setState({member});
+    });
+
+    const room = this.drone.subscribe("observable-room-chat");
+    room.on("data", (data, member) => {
+      const messages = this.state.text;
+      messages.push({member, text: data});
+      this.setState({messages});
+    });
+  
+}
+
+ sendMessage = (message) => {
+  this.drone.publish({
+    room: "observable-room-chat",
+    message
+  });
+  this.setState({message});
+}
+
   render() {
+
     return (
       <div className="App">
-        <div className="App-header">
           <h1>Chat</h1>
-        </div>
+        <Messages
+          messages={this.state.text}
+          currentMember={this.state.member}
+          />
+        <Input onSendMessage={this.sendMessage}/>
       </div>
     );
   }
--- src/components/Input.js
+++ src/components/Input.js
@@ -1,1 +1,28 @@
 import React from 'react';
+import {useState} from 'react';
+
+export default function Input({onSendMessage}) {
+
+const [text, setText] = useState('');
+
+// Input text value
+const changeInput = (e) => {
+    const newText = e.target.value;
+    setText(newText.trimStart());
+}
+
+// Submiting text and preventing from refreshing
+const handleSubmit = (e) => {
+    e.preventDefault();
+    onSendMessage(text);
+    setText('')
+}
+
+return (
+        <form onSubmit={handleSubmit}>
+            <input onChange={changeInput} value={text} type="text"
+                placeholder="Write something..">
+            </input>
+        </form>
+    );
+}
